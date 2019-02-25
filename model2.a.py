60be7970af842b7bfa593ba52adf80d26440c429
jassalak <akash.jassal09@gmail.com>
2019-02-25 13:02:53 -0500

    Update model2.a.py

--- model2.a.py
+++ model2.a.py
@@ -32,9 +32,10 @@ df1_flu16 = pd.read_excel('FluShots20162017v2.xlsx',sheet_name='2016')
 df1_flu16.columns.values[7] = "Pt Ins"
 df1_flu16 = df1_flu16.drop(['Diagnosis Codes','Diagnosis Codes_1'], axis =1)
 df1_flu16.rename(columns={'Diagnosis Codes_2':'Diagnosis Codes','Account Number':'AcctNum'}, inplace=True)
+df1_flu16['year'] = 2016
 df1_flu17 = pd.read_excel('FluShots20162017v2.xlsx',sheet_name='2017')
 df1_flu17.rename(columns={'Pt Address 1':'Pt Address 2','CPT Description':'Pt Address 1','CPT.1':'CPT Description','Account Number':'AcctNum'}, inplace=True)
-
+df1_flu17['year'] = 2017
 df1_flu1617 = pd.concat([df1_flu16,df1_flu17], sort = False)
 df1_flu1617 = df1_flu1617[pd.to_numeric(df1_flu1617['AcctNum'], errors='coerce').notnull()]
 df1_flu1617['AcctNum'] = pd.to_numeric(df1_flu1617['AcctNum'])
@@ -54,13 +55,30 @@ df2_fluDx =df2_fluDx.reset_index(drop = True)
 df1 = pd.merge(df1_flu1617,df2_fluDx,left_on = 'AcctNum',right_on = 'AcctNum', how = 'left')
 df1['FluDx_YES'].replace(np.nan,'0',inplace=True)
 df1['FluDx_YES'] = pd.to_numeric(df1['FluDx_YES'])
-
+df1.columns = df1.columns.str.upper()
+#df1.to_excel("df1.xlsx")
+df1 = pd.read_excel('df1.xlsx',sheet_name='Sheet1')
 ################################################################################
+
+df2 = df1.copy(deep = False)
+df2 = df1.drop(['ACCTNUM','PCP','LMG PRACTICE','DOS','DIAGNOSIS CODES','PLACE OF SERVICE','CPT', 'CPT DESCRIPTION','PT ADDRESS 1','PT ADDRESS 2', 'PT ZIP', 'PT DOB'], axis=1)
+df2_qual = [f for f in df2.columns if df2.dtypes[f] == 'object']
+df2_quant = [f for f in df2.columns if df2.dtypes[f] != 'object']
+df2_feats = list(df2)
+
+for f in df2.columns:
+    print(df2[f].value_counts())
+    
+print(df2['PCP SPECIALTY'].value_counts())
+print(df2['PT INS'].value_counts())
+print(df2['PT STATE'].value_counts())
+print(df2['PT CITY'].value_counts())
 
 ### DV Density
 plt.figure(2); plt.title('Normal')
 sns.distplot(df1['FluDx_YES'], kde=False, fit=st.norm)
 
 ### Extra code
+#https://www.datacamp.com/community/tutorials/categorical-data
 #df3_fluDx.replace('',np.nan,inplace=True)
 #df3_fluDx.dropna(inplace=True)
