d846e8f2e1b8170a3ce0eba37f803e2fd97eb5b5
jassalak <akash.jassal09@gmail.com>
2019-03-31 12:58:21 -0400

    Update model3.a.py

--- model3.a.py
+++ model3.a.py
@@ -100,22 +100,26 @@ for f in df2.columns:
 ################################################################################
 
 # Lower Limit Thresholds
+pt_state_lt = 1
+pcp_speciality_lt = 16
+pt_ins_lt = 1
 pt_race_lt = 5
+cpt_lt = 7
 
 ### Combining Variables into Other
 series = pd.value_counts(df2['PT STATE'])
 mask = (series/series.sum() * 100)                                              # To replace df['column'] use np.where I.e
-mask = (series/series.sum() * 100).lt(1)                                        # lt(%); where % is the cut off
+mask = (series/series.sum() * 100).lt(pt_state_lt)                                        # lt(%); where % is the cut off
 df2['PT STATE'] = np.where(df2['PT STATE'].isin(series[mask].index),'Other',df2['PT STATE'])
 
 series = pd.value_counts(df2['PCP SPECIALTY'])
 mask = (series/series.sum() * 100)                                              # To replace df['column'] use np.where I.e
-mask = (series/series.sum() * 100).lt(16)                                        # lt(%); where % is the cut off
+mask = (series/series.sum() * 100).lt(pcp_speciality_lt)                                        # lt(%); where % is the cut off
 df2['PCP SPECIALTY'] = np.where(df2['PCP SPECIALTY'].isin(series[mask].index),'Other',df2['PCP SPECIALTY'])
 
 series = pd.value_counts(df2['PT INS'])
 mask = (series/series.sum() * 100)
-mask = (series/series.sum() * 100).lt(1)                                        # lt(%); where % is the cut off
+mask = (series/series.sum() * 100).lt(pt_ins_lt)                                        # lt(%); where % is the cut off
 df2['PT INS'] = np.where(df2['PT INS'].isin(series[mask].index),'Other',df2['PT INS'])
 
 df2['PT RACE'].replace('_R','Other',inplace=True)                               # DOES NOT WORK WELL, DUPE OF 210 and 202
@@ -126,7 +130,7 @@ df2['PT RACE'] = np.where(df2['PT RACE'].isin(series[mask].index),'Other',df2['P
 
 series = pd.value_counts(df2['CPT'])
 mask = (series/series.sum() * 100)
-mask = (series/series.sum() * 100).lt(7)                                        # lt(%); where % is the cut off
+mask = (series/series.sum() * 100).lt(cpt_lt)                                        # lt(%); where % is the cut off
 df2['CPT'] = np.where(df2['CPT'].isin(series[mask].index),'Other',df2['CPT'])
 
 new = series[~mask]
@@ -144,7 +148,7 @@ df2_feats = list(df2)
 print(df2.groupby('FLUDX_YES').mean() )
 
 ################################################################################
-
+# DF3
 # Import `PCA` from `sklearn.decomposition`
 
 traincols =['Yr_2016','Yr_2017','Gndr_F','Gndr_M',
@@ -173,6 +177,19 @@ df3 = pd.DataFrame(data=df3, columns = traincols).reset_index(drop = True)
 
 ################################################################################
 
+# DF4
+# Import `PCA` from `sklearn.decomposition`
+
+traincols =['Gndr_F','Gndr_M',
+            'State_MD','State_Other','State_VA','State_WV',
+            'Spclty_Family Practice','Spclty_Internal Medicine','Spclty_Other',
+            'Ins_AETNA','Ins_BCBS','Ins_CIGNA','Ins_MCAID','Ins_MCARE','Ins_Other','Ins_TRICARE','Ins_UNITED',
+            'Age_1','Age_2','Age_3','Age_4','Age_5']
+
+df4 = df2_quant.copy(deep = True)
+df4 = df4[traincols].astype(float)
+
+################################################################################
 ### IV Correlation to DV
 print("Find most important features relative to DV")
 df2_corr = df2.corr()
@@ -255,6 +272,41 @@ Y_train_smote.rename(columns={0:'FLUDX_YES'}, inplace=True)
 
 ################################################################################
 
+### SMOTE (df4)
+
+traincols =['Gndr_F','Gndr_M',
+            'State_MD','State_Other','State_VA','State_WV',
+            'Spclty_Family Practice','Spclty_Internal Medicine','Spclty_Other',
+            'Ins_AETNA','Ins_BCBS','Ins_CIGNA','Ins_MCAID','Ins_MCARE','Ins_Other','Ins_TRICARE','Ins_UNITED',
+            'Age_1','Age_2','Age_3','Age_4','Age_5']
+
+y = pd.DataFrame(df2['FLUDX_YES'].astype(float))
+x = df2[traincols].astype(float)
+
+X_train, X_test, Y_train, Y_test = tts(x, y, test_size=0.3, random_state=5026)
+
+print("Number transactions X_train dataset: ", X_train.shape)
+print("Number transactions y_train dataset: ", Y_train.shape)
+print("Number transactions X_test dataset: ", X_test.shape)
+print("Number transactions y_test dataset: ", Y_test.shape)
+
+
+sm = SMOTE(random_state=5026)
+X_train_smote, Y_train_smote = sm.fit_sample(X_train, Y_train.values.ravel())
+
+print('After OverSampling, the shape of train_X: {}'.format(X_train_smote.shape))
+print('After OverSampling, the shape of train_y: {} \n'.format(Y_train_smote.shape))
+
+print("After OverSampling, counts of label '1': {}".format(sum(Y_train_smote==1)))
+print("After OverSampling, counts of label '0': {}".format(sum(Y_train_smote==0)))
+
+
+X_train_smote = pd.DataFrame(data=X_train_smote, columns = traincols).reset_index(drop = True)
+
+Y_train_smote = pd.DataFrame(data = Y_train_smote).reset_index(drop = True)
+Y_train_smote.rename(columns={0:'FLUDX_YES'}, inplace=True)
+################################################################################
+
 ### DV Density
 plt.figure(2); plt.title('Normal')
 sns.distplot(df2['FLUDX_YES'], kde=False, fit=st.norm)
@@ -280,13 +332,13 @@ logit_yhat = np.where(logit_prob[:,1] >= logit_threshold,1,0)
 
 
 logit_score =  round(metrics.accuracy_score(Y_test, logit_yhat)*100,2)
-print('\n Score logit:', metrics.accuracy_score(Y_test, logit_yhat) )
+print('\n Score logit:', metrics.accuracy_score(Y_test, logit_yhat) ) #0.8584547181760608
 #print(' \n Intercept logit: ',logit.intercept_)
 logit_coef = pd.DataFrame(logit.coef_[0], X_test.columns, columns=['logit_Coefficients'])
 logit_confusion_matrix = pd.DataFrame(metrics.confusion_matrix(Y_test, logit_yhat), columns=['predicted 0','predicted 1'], index =['actual 0','actual 1'] )
 print('\n Confusion Matrix logit SMOTE: \n',logit_confusion_matrix)
 logit_auc = metrics.roc_auc_score(Y_test, logit_yhat)
-print('\n AUC: \n', logit_auc) #0.5688
+print('\n AUC: \n', logit_auc)  #0.568841245785207
 
 ### Evaluation Metrics
 tn = logit_confusion_matrix.iloc[0,0]
@@ -316,13 +368,50 @@ logit_yhat = np.where(logit_prob[:,1] >= logit_threshold,1,0)
 
 
 logit_score =  round(metrics.accuracy_score(Y_test, logit_yhat)*100,2)
-print('\n Score logit:', metrics.accuracy_score(Y_test, logit_yhat) )
+print('\n Score logit:', metrics.accuracy_score(Y_test, logit_yhat) ) #0.880050664977834
 #print(' \n Intercept logit: ',logit.intercept_)
 logit_coef = pd.DataFrame(logit.coef_[0], X_test.columns, columns=['logit_Coefficients'])
 logit_confusion_matrix = pd.DataFrame(metrics.confusion_matrix(Y_test, logit_yhat), columns=['predicted 0','predicted 1'], index =['actual 0','actual 1'] )
 print('\n Confusion Matrix logit SMOTE: \n',logit_confusion_matrix)
 logit_auc = metrics.roc_auc_score(Y_test, logit_yhat)
 print('\n AUC: \n', logit_auc)  #  0.558901607998342
+
+### Evaluation Metrics
+tn = logit_confusion_matrix.iloc[0,0]
+fp = logit_confusion_matrix.iloc[0,1]
+fn = logit_confusion_matrix.iloc[1,0]
+tp = logit_confusion_matrix.iloc[1,1]
+sensitivity = tp/(tp+fn)*100                                                    #print(sensitivity) # this percent... of all True values, the model was able to predict
+specificity = tn / (tn + fp) *100                                               #print(specificity) # this percent... of all False values, the model was able to predict
+################################################################################
+
+#### Logistic Regression (sklearn) DF4
+#df3 = df2_quant.copy(deep = False)
+# prepare X and y
+#x = df3.drop(['FLUDX_YES','UNITS','PTAGE'],axis=1,inplace=False) # PATIENT AGE!!!!!!!!!!!!!!!!!
+#y = df3[['FLUDX_YES']]
+
+#X_train, X_test, Y_train, Y_test =tts(x, y, test_size = 0.3, random_state=5026)
+
+logit = LogisticRegression()
+result = logit.fit(X_train,Y_train)
+#print(result.summary())
+
+logit_yhat = logit.predict(X_test)
+logit_prob = logit.predict_proba(X_test)
+logit_ci90 = (np.percentile(logit_prob[:,1],90))
+logit_threshold = logit_ci90
+logit_yhat = np.where(logit_prob[:,1] >= logit_threshold,1,0)
+
+
+logit_score =  round(metrics.accuracy_score(Y_test, logit_yhat)*100,2)
+print('\n Score logit:', metrics.accuracy_score(Y_test, logit_yhat) ) #0.8235592146928435
+#print(' \n Intercept logit: ',logit.intercept_)
+logit_coef = pd.DataFrame(logit.coef_[0], X_test.columns, columns=['logit_Coefficients'])
+logit_confusion_matrix = pd.DataFrame(metrics.confusion_matrix(Y_test, logit_yhat), columns=['predicted 0','predicted 1'], index =['actual 0','actual 1'] )
+print('\n Confusion Matrix logit SMOTE: \n',logit_confusion_matrix)
+logit_auc = metrics.roc_auc_score(Y_test, logit_yhat)
+print('\n AUC: \n', logit_auc)  #  0.584751342
 
 ### Evaluation Metrics
 tn = logit_confusion_matrix.iloc[0,0]
@@ -353,7 +442,7 @@ logit_yhat = np.where(logit_prob[:,1] >= logit_threshold,1,0)
 
 
 logit_score =  round(metrics.accuracy_score(Y_test, logit_yhat)*100,2)
-print('\n Score logit:', metrics.accuracy_score(Y_test, logit_yhat) )
+print('\n Score logit:', metrics.accuracy_score(Y_test, logit_yhat) ) #0.8560481317289423
 #print(' \n Intercept logit: ',logit.intercept_)
 logit_coef = pd.DataFrame(logit.coef_[0], X_test.columns, columns=['logit_Coefficients'])
 logit_confusion_matrix = pd.DataFrame(metrics.confusion_matrix(Y_test, logit_yhat), columns=['predicted 0','predicted 1'], index =['actual 0','actual 1'] )
@@ -391,7 +480,7 @@ logit_yhat = np.where(logit_prob[:,1] >= logit_threshold,1,0)
 
 
 logit_score =  round(metrics.accuracy_score(Y_test, logit_yhat)*100,2)
-print('\n Score logit:', metrics.accuracy_score(Y_test, logit_yhat) )
+print('\n Score logit:', metrics.accuracy_score(Y_test, logit_yhat) ) #0.8797973400886637
 #print(' \n Intercept logit: ',logit.intercept_)
 logit_coef = pd.DataFrame(logit.coef_[0], X_test.columns, columns=['logit_Coefficients'])
 logit_confusion_matrix = pd.DataFrame(metrics.confusion_matrix(Y_test, logit_yhat), columns=['predicted 0','predicted 1'], index =['actual 0','actual 1'] )
@@ -407,6 +496,45 @@ tp = logit_confusion_matrix.iloc[1,1]
 sensitivity = tp/(tp+fn)*100                                                    #print(sensitivity) # this percent... of all True values, the model was able to predict
 specificity = tn / (tn + fp) *100                                               #print(specificity) # this percent... of all False values, the model was able to predict
 ################################################################################
+
+#### Logistic Regression (sklearn) SMOTE df4
+#df3 = df2_quant.copy(deep = False)
+
+# prepare X and y
+#x = df3.drop(['FLUDX_YES','UNITS'],axis=1,inplace=False)
+#y = df3[['FLUDX_YES']]
+
+#X_train, X_test, Y_train, Y_test =tts(x, y, test_size = 0.3, random_state=5026)
+
+
+logit = LogisticRegression()
+result = logit.fit(X_train_smote,Y_train_smote)
+
+logit_yhat = logit.predict(X_test)
+logit_prob = logit.predict_proba(X_test)
+logit_ci90 = (np.percentile(logit_prob[:,1],90))
+logit_threshold = logit_ci90
+logit_yhat = np.where(logit_prob[:,1] >= logit_threshold,1,0)
+
+
+logit_score =  round(metrics.accuracy_score(Y_test, logit_yhat)*100,2)
+print('\n Score logit:', metrics.accuracy_score(Y_test, logit_yhat) ) #0.8310956301456618
+#print(' \n Intercept logit: ',logit.intercept_)
+logit_coef = pd.DataFrame(logit.coef_[0], X_test.columns, columns=['logit_Coefficients'])
+logit_confusion_matrix = pd.DataFrame(metrics.confusion_matrix(Y_test, logit_yhat), columns=['predicted 0','predicted 1'], index =['actual 0','actual 1'] )
+print('\n Confusion Matrix logit: \n',logit_confusion_matrix)
+logit_auc = metrics.roc_auc_score(Y_test, logit_yhat)
+print('\n AUC: \n', logit_auc) #0.5785534541374721
+
+### Evaluation Metrics
+tn = logit_confusion_matrix.iloc[0,0]
+fp = logit_confusion_matrix.iloc[0,1]
+fn = logit_confusion_matrix.iloc[1,0]
+tp = logit_confusion_matrix.iloc[1,1]
+sensitivity = tp/(tp+fn)*100                                                    #print(sensitivity) # this percent... of all True values, the model was able to predict
+specificity = tn / (tn + fp) *100                                               #print(specificity) # this percent... of all False values, the model was able to predict
+################################################################################
+
 #df2
 # SVC -->  https://elitedatascience.com/imbalanced-classes
 # Separate input features (X) and target variable (y)
@@ -455,12 +583,40 @@ svc_yhat = svc.predict(X_test)
 print( np.unique( svc_yhat ) )
 
 # How's our accuracy?
-print( metrics.accuracy_score(Y_test, svc_yhat) ) #
+print( metrics.accuracy_score(Y_test, svc_yhat) ) #0.5300823305889804
 
 # What about AUROC?
 svc_prob = svc.predict_proba(X_test)
 svc_prob = [p[1] for p in svc_prob]
-print( roc_auc_score(Y_test, svc_prob) ) #
+print( roc_auc_score(Y_test, svc_prob) ) #0.6780682796470335
+
+##############################################################################
+#df4
+# SVC -->  https://elitedatascience.com/imbalanced-classes
+# Separate input features (X) and target variable (y)
+#y = df.balance
+#X = df.drop('balance', axis=1)
+
+# Train model
+svc = SVC(kernel='linear',
+            class_weight='balanced', # penalize
+            probability=True)
+
+svc.fit(X_train, Y_train)
+
+# Predict on training set
+svc_yhat = svc.predict(X_test)
+
+# Is our model still predicting just one class?
+print( np.unique( svc_yhat ) )
+
+# How's our accuracy?
+print( metrics.accuracy_score(Y_test, svc_yhat) ) #0.5300823305889804
+
+# What about AUROC?
+svc_prob = svc.predict_proba(X_test)
+svc_prob = [p[1] for p in svc_prob]
+print( roc_auc_score(Y_test, svc_prob) ) #0.6764161779300285
 
 ##############################################################################
 # SVC -->  SMOTE DF2
@@ -518,7 +674,34 @@ svc_prob = [p[1] for p in svc_prob]
 print( roc_auc_score(Y_test, svc_prob) ) #
 ##############################################################################
 
-# Tree Model -->
+# SVC -->  SMOTE DF4
+# Separate input features (X) and target variable (y)
+#y = df.balance
+#X = df.drop('balance', axis=1)
+
+# Train model
+svc = SVC(kernel='linear',
+            class_weight='balanced', # penalize
+            probability=True)
+
+svc.fit(X_train_smote, Y_train_smote)
+
+# Predict on training set
+svc_yhat = svc.predict(X_test)
+
+# Is our model still predicting just one class?
+print( np.unique( svc_yhat ) )
+
+# How's our accuracy?
+print( metrics.accuracy_score(Y_test, svc_yhat) ) #0.5394553514882837
+
+# What about AUROC?
+svc_prob = svc.predict_proba(X_test)
+svc_prob = [p[1] for p in svc_prob]
+print( roc_auc_score(Y_test, svc_prob) ) #0.661868789906019
+##############################################################################
+
+# Tree Model --> df2
 # Separate input features (X) and target variable (y)
 #y = df.balance
 #X = df.drop('balance', axis=1)
@@ -545,7 +728,7 @@ print( roc_auc_score(Y_test, tree_prob) ) # 0.6793665953240706
 
 
 ################################################################################
-# Tree Model --> SMOTE
+# Tree Model --> SMOTE df2
 # Separate input features (X) and target variable (y)
 #y = df.balance
 #X = df.drop('balance', axis=1)
@@ -590,14 +773,12 @@ print( np.unique( tree_yhat ) )
 
 
 # How's our accuracy?
-print( metrics.accuracy_score(Y_test, tree_yhat) ) #
-
+print( metrics.accuracy_score(Y_test, tree_yhat) ) #0.966624446
 
 # What about AUROC?
 tree_prob = tree.predict_proba(X_test)
 tree_prob = [p[1] for p in tree_prob]
-print( roc_auc_score(Y_test, tree_prob) ) #
-
+print( roc_auc_score(Y_test, tree_prob) ) # 0.683327554
 
 ################################################################################
 
@@ -618,18 +799,72 @@ print( np.unique( tree_yhat ) )
 
 
 # How's our accuracy?
-print( metrics.accuracy_score(Y_test, tree_yhat) ) #
+print( metrics.accuracy_score(Y_test, tree_yhat) ) # 0.6377454084863838
 
 
 # What about AUROC?
 tree_prob = tree.predict_proba(X_test)
 tree_prob = [p[1] for p in tree_prob]
-print( roc_auc_score(Y_test, tree_prob) ) #
+print( roc_auc_score(Y_test, tree_prob) ) # 0.6811904523479286
 
 
 ################################################################################
 
+################################################################################
 
+# Tree Model --> df4
+# Separate input features (X) and target variable (y)
+#y = df.balance
+#X = df.drop('balance', axis=1)
+
+# Train model
+tree = RandomForestClassifier()
+tree.fit(X_train, Y_train)
+
+# Predict on training set
+tree_yhat = tree.predict(X_test)
+
+# Is our model still predicting just one class?
+print( np.unique( tree_yhat ) )
+
+
+# How's our accuracy?
+print( metrics.accuracy_score(Y_test, tree_yhat) ) #0.9664344521849272
+
+# What about AUROC?
+tree_prob = tree.predict_proba(X_test)
+tree_prob = [p[1] for p in tree_prob]
+print( roc_auc_score(Y_test, tree_prob) ) # 0.6912616155909479
+
+################################################################################
+
+# Tree Model --> SMOTE df4
+# Separate input features (X) and target variable (y)
+#y = df.balance
+#X = df.drop('balance', axis=1)
+
+# Train model
+tree = RandomForestClassifier()
+tree.fit(X_train_smote, Y_train_smote)
+
+# Predict on training set
+tree_yhat = tree.predict(X_test)
+
+# Is our model still predicting just one class?
+print( np.unique( tree_yhat ) )
+
+
+# How's our accuracy?
+print( metrics.accuracy_score(Y_test, tree_yhat) ) # 0.5394553514882837
+
+
+# What about AUROC?
+tree_prob = tree.predict_proba(X_test)
+tree_prob = [p[1] for p in tree_prob]
+print( roc_auc_score(Y_test, tree_prob) ) # 0.661868789906019
+
+
+################################################################################
 
 
 
